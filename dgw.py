f4966289a0e8538f76393036653d0f6b4a89c107
silverware13 <thomasza@oregonstate.edu>
2018-02-12 19:28:14 -0800

    Added buttons

--- dgw.py
+++ dgw.py
@@ -1,30 +1,56 @@
 # Digital Ghostwriter
-#
-# Zachary Thomas
-# thomasza@oregonstate.edu
 # 2/12/2018
-#
 
-import sys
-#from tkinter import *
+from collections import namedtuple
 import tkinter as tk
 
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
+Rect = namedtuple('Rect', 'x0, y0, x1, y1')
 
-# Set window size and name.
-root.geometry("829x556")
-root.title("Digital Ghostwriter")
+class ImageMapper(object):
+    def __init__(self, image, img_rects):
+        self.width, self.height = image.width(), image.height()
+        self.img_rects = img_rects
 
-# Set image.
-image = tk.PhotoImage(file="MM.gif")
-label = tk.Label(image=image)
-label.image = image
-label.pack()
+    def find_rect(self, x, y):
+        for i, r in enumerate(self.img_rects):
+            if (r.x0 <= x <= r.x1) and (r.y0 <= y <= r.y1):
+                return i
+        return None
 
-# Exit button.
-button = tk.Button(frame, text="QUIT", fg="red", command=quit)
-button.pack(side=tk.LEFT)
+class Demo(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.create_widgets()
 
-root.mainloop()
+    def create_widgets(self):
+        self.msg_text = tk.StringVar()
+        self.msg = tk.Message(self, textvariable=self.msg_text, width=100)
+        self.msg.grid(row=0, column=0)
+
+        self.picture = tk.PhotoImage(file='MM.gif')
+	# left, top, right, bottom.
+	# 0: Exit button.
+	# 1: Genre button.
+	# 2: Write button.
+        img_rects = [
+		     Rect(750, 0, 829, 64),
+                     Rect(0, 65, 413, 556),
+                     Rect(414, 65, 829, 556)]
+        self.imagemapper = ImageMapper(self.picture, img_rects)
+        # use Label widget to display image
+        self.image = tk.Label(self, image=self.picture, borderwidth=0)
+        self.image.bind('<Button-1>', self.image_click)
+        self.image.grid(row=1, column=0)
+
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.grid(row=2, column=0)
+
+    def image_click(self, event):
+        hit = self.imagemapper.find_rect(event.x, event.y)
+        self.msg_text.set('{} clicked'.format('nothing' if hit is None else
+                                              'rect[{}]'.format(hit)))
+
+app = Demo()
+app.master.title('Image Mapper')
+app.mainloop()
