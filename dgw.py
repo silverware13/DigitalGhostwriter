bb01c0f71e97943a2dd3390045a1ff2e4df7502c
silverware13 <thomasza@oregonstate.edu>
2018-02-13 15:50:13 -0800

    Pages added.

--- dgw.py
+++ dgw.py
@@ -6,44 +6,41 @@ import tkinter as tk
 
 Rect = namedtuple('Rect', 'x0, y0, x1, y1')
 
-class ImageMapper(object):
-    def __init__(self, image, img_rects):
-        self.width, self.height = image.width(), image.height()
-        self.img_rects = img_rects
+## TEST
 
-    def find_rect(self, x, y):
-        for i, r in enumerate(self.img_rects):
-            if (r.x0 <= x <= r.x1) and (r.y0 <= y <= r.y1):
-                return i
-        return None
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
 
-class Demo(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.create_widgets()
+class Page1(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       self.grid()
+       self.create_widgets()
 
-    def create_widgets(self):
-        self.msg_text = tk.StringVar()
-        self.msg = tk.Message(self, textvariable=self.msg_text, width=100)
-        self.msg.grid(row=0, column=0)
+   def create_widgets(self):
+       self.msg_text = tk.StringVar()
+       self.msg = tk.Message(self, textvariable=self.msg_text, width=100)
+       self.msg.grid(row=0, column=0)
 	
-        self.picture = tk.PhotoImage(file='MM.gif')
+       self.picture = tk.PhotoImage(file='MM.gif')
 	# left, top, right, bottom.
 	# 0: Exit button.
 	# 1: Genre button.
 	# 2: Write button.
-        img_rects = [
-		     Rect(750, 0, 829, 64),
-                     Rect(0, 65, 413, 556),
-                     Rect(414, 65, 829, 556)]
-        self.imagemapper = ImageMapper(self.picture, img_rects)
+       img_rects = [
+              Rect(750, 0, 829, 64),
+              Rect(0, 65, 413, 556),
+              Rect(414, 65, 829, 556)]
+       self.imagemapper = ImageMapper(self.picture, img_rects)
         # use Label widget to display image
-        self.image = tk.Label(self, image=self.picture, borderwidth=0)
-        self.image.bind('<Button-1>', self.image_click)
-        self.image.grid(row=1, column=0)
+       self.image = tk.Label(self, image=self.picture, borderwidth=0)
+       self.image.bind('<Button-1>', self.image_click)
+       self.image.grid(row=1, column=0)
 
-    def image_click(self, event):
+   def image_click(self, event):
         hit = self.imagemapper.find_rect(event.x, event.y)
         # If the user clicks the exit button, quit.
         if (hit == 0):
@@ -59,6 +56,64 @@ class Demo(tk.Frame):
             Screen=0
             self.msg_text.set('Nothing selected.')
 
-app = Demo()
+class Page2(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 2!")
+       label.pack(side="top", fill="both", expand=True)
+
+class Page3(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 3")
+       label.pack(side="top", fill="both", expand=True)
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page1(self)
+        p2 = Page2(self)
+        p3 = Page3(self)
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(buttonframe, text="Page 1", command=p1.lift)
+        b2 = tk.Button(buttonframe, text="Page 2", command=p2.lift)
+        b3 = tk.Button(buttonframe, text="Page 3", command=p3.lift)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+
+        p1.show()
+
+## TEST
+
+class ImageMapper(object):
+    def __init__(self, image, img_rects):
+        self.width, self.height = image.width(), image.height()
+        self.img_rects = img_rects
+
+    def find_rect(self, x, y):
+        for i, r in enumerate(self.img_rects):
+            if (r.x0 <= x <= r.x1) and (r.y0 <= y <= r.y1):
+                return i
+        return None
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("400x400")
+    root.mainloop()
+
+appo= Demo()
 app.master.title('Digital Ghostwriter')
 app.mainloop()
